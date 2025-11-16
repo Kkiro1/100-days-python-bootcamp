ff248d5239f849e2dc5938606ebba4aa6349ad7f
Kerollos Emad <kerollose543@gmail.com>
2025-11-16 19:38:30 +0200

    fix typo in day 1

--- Day 1 - Band Name Generator/main.py
+++ Day 1 - Band Name Generator/main.py
@@ -4,4 +4,4 @@ city_name = input("What's the name of the city you grew up in? ")
 pet_name = input("What's the name of your pet? ")
 
 band_name = f"{city_name} {pet_name}"
-print(f"Your bane name could be: {band_name}")
+print(f"Your band name could be: {band_name}")
